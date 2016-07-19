fae40bd7a4c8fc716183926fb8fa43b2c130462a
vtleavs <vtleavs@gmail.com>
2016-07-19 12:13:35 -0400

    Added step_angle field to Settings

--- StarshipFirmware_RAMPS-1.4_Intrepid/Settings.h
+++ StarshipFirmware_RAMPS-1.4_Intrepid/Settings.h
@@ -1,8 +1,11 @@
 
 struct Settings // contains machine and build settings
 {
-    Settings(): units("mm") {}
+    Settings(): 
+        units("mm")
+        step_angle(1.8){}
     String units;
+    float step_angle;
 };
 
 Settings settings;
